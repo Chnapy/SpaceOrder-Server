04f063e82b555528d2e67e335d5b2c82b3775d47
Richard <richardhaddad@hotmail.fr>
2017-07-10 23:35:34 +0200

    Dependances

--- app.ts
+++ app.ts
@@ -71,14 +71,14 @@ export default class App {
     }
 
     private setupErrorHandler() {
-        this.app.use((err, req, res, next) => {
+        this.app.use((err, req, res) => {
             // set locals, only providing error in development
             res.locals.message = err.message;
             res.locals.error = req.app.get('env') === 'development' ? err : {};
 
             // render the error page
             res.status(err.status || 500);
-            res.render('error');
+            res.send('error');
         });
     }
 
